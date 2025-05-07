aeb4b1a9fd93d03e648fe8095d8f1a5ca54e348e
mahmoud ezzat <mmezzat23@gmail.com>
2025-05-07 13:40:52 +0300

    splash + select type + language change design

---
+++ app/src/main/java/fudex/bonyad/ui/Activity/SelecttypeActivity.kt
@@ -0,0 +1,12 @@
+package fudex.bonyad.ui.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fudex.bonyad.R
+
+class SelecttypeActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selecttype)
+    }
+}
