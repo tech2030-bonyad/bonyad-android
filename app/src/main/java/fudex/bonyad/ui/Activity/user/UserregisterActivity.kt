9b07b507adbfc20b2dc4784889e74a0731d75acb
mahmoud ezzat <mmezzat23@gmail.com>
2025-05-07 16:05:01 +0300

    active user + forget password + reset password design

---
+++ app/src/main/java/fudex/bonyad/ui/Activity/user/UserregisterActivity.kt
@@ -0,0 +1,16 @@
+package fudex.bonyad.ui.Activity.user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import fudex.bonyad.R
+import fudex.bonyad.ui.Activity.BaseActivity
+
+class UserregisterActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_userregister)
+    }
+}
